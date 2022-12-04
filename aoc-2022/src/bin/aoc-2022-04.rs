52c7bb823c70822374374a38c3e3ac929419db09
Jason Birch <jbirch@jbirch.net>
2022-12-04 21:46:01 +1100

    AOC-2022-04: Not doing this now, but here's some skeletons for later.

---
+++ aoc-2022/src/bin/aoc-2022-04.rs
@@ -0,0 +1,18 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one() {
+
+    }
+
+    #[test]
+    fn test_two() {
+
+    }
+}
